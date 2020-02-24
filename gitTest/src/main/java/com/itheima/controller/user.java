0fe138c7ea7232e92034c87d52e8da1069000eff
Administrator <xx073311>
2020-02-24 19:10:14 +0800

    提交到本地仓库

---
+++ gitTest/src/main/java/com/itheima/controller/user.java
@@ -0,0 +1,14 @@
+package com.itheima.controller;
+
+/**
+ * 包名: user
+ * 作者:XUANDAREN
+ * 日期2020/2/2419:01
+ */
+public class user {
+
+    public String getName(){
+
+        return "马云";
+    }
+}
